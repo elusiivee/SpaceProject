017d4be41717de58fbadffb7d4c1471e4d43e718
elusiivee <andrii.1669@gmail.com>
2023-08-21 20:57:51 +0200

    fixed some issues and created page with Space technology

---
+++ SpaceProject/src/components/Technology.vue
@@ -0,0 +1,366 @@
+<script >
+
+  
+import $ from 'jquery';
+
+
+export default {
+  data() {
+    return {
+      cardData: [],
+    };
+  },
+  methods: {
+    openModal() {
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    },
+  },
+
+  // fetching
+  mounted() {
+    fetch('https://api.nasa.gov/techtransfer/patent/?engine&api_key=eJMyDTpOHHfmxGjf8yuuWMnfDxZIMuI3pk56Nh8U')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        const importantData = data.results;
+        console.log(importantData)
+        this.getValue(importantData);
+      })
+      .catch(error => {
+        console.error("Fetch error:", error);
+      });
+  },
+  methods: {
+    getValue(data) {
+      for (let i = 0; i < data.length; i++) {
+        const item = data[i];
+        const plainDescription = item[3].replace(/<\/?span[^>]*>/g, ''); // Remove <span> tags
+        this.cardData.push({
+          description: plainDescription,
+          image: item[10],
+          showModal: false, 
+        });
+        ;
+      }
+    },
+    limit(text, number){
+        if (text.length>number){
+          return text.slice(0,number)+'...';
+        }
+        else{
+          return text;
+        }
+    },
+    openModal(card) {
+      card.showModal = true;
+    },
+    closeModal(card) {
+      card.showModal = false;
+    },
+    toHomePage() {
+      this.$router.push('/');
+    },
+  },
+}
+
+//create max length of string
+
+var maxLength = 15;
+$('description').keyup(function() {
+  var textlen = maxLength - $(this).val().length;
+  $('#maxchars').text(textlen);})
+</script>
+
+<template>
+<div class="my-body">
+
+<nav class="main-nav">
+  <div class="nav-bg"></div>
+  <ul>
+    <li><a href="" @click="toHomePage()">Home</a></li>
+    <li><a href="">About</a></li>
+  </ul>
+</nav>
+<header>
+  <div class="overlay">
+    <p style="margin-top: 10rem ;">Space Technology</p>
+    </div>
+</header>
+
+<div class="content-wrapper normal_text">
+  <h1 class="boldtext">NASA's Technology Transfer Program</h1>
+    
+  <p>innovations forged in the crucible of exploration and discovery are seamlessly integrated into the fabric of our daily lives. This program exemplifies NASA's dedication to the broader public, making its rich patent portfolio available for the benefit of the United States' citizens. By fostering partnerships and entering into licensing agreements with various industries, these patents become the bedrock upon which a thriving economy is built, jobs are created, and the overall quality of life is elevated.</p>
+
+  <p>At its core, this program epitomizes the spirit of pioneering research. It bridges the gap between the frontiers of space and the realms of practical application, ensuring that the monumental investments made by NASA in pushing the boundaries of knowledge find resonating echoes in the advancement of our society. These innovations, often birthed in the pursuit of understanding the cosmos, don a new role as catalysts for transformative change in sectors ranging from technology and healthcare to transportation and energy.</p>
+
+  <div class="orange-line"></div>
+  <div class="row">
+    <div v-for="card in cardData" :key="card.description" class="col-3" style="margin-bottom: 1rem;">
+      <div class="card cart-descr normal_text">
+        <img :src="card.image" :alt="card.description" style="margin-bottom: 0.5rem;" />
+        <p>{{ limit(card.description, 100) }}</p>
+        <button class="cart_btn" @click="openModal(card)">More</button>
+        <div v-if="card.showModal" class="modal-overlay">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button @click="closeModal(card)" class="cart_btn closed">Close</button>
+            </div>
+            <div class="modal-body">
+              {{ card.description }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+
+</div>
+</div>
+
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Catamaran:wght@300;700&family=Trirong:wght@700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Catamaran:wght@300;700&family=Trirong:wght@400;700&display=swap');
+
+.normal_text{
+  font-family: Trirong;
+    font-weight: 700;  
+    font-size: 1rem;
+    text-indent: 1.5em;
+}
+
+.boldtext{
+  font-family: Trirong;
+  font-weight: 700;  
+  font-size: 2rem;
+  letter-spacing: 1px;
+  color: rgb(220, 120, 0);
+}
+
+
+
+
+.my-body {
+  margin: 0;
+  padding: 0;
+
+  /* background-image: url('./icons/bdstarsdark2.jpg') ; */
+
+  background-size: 100% ;
+  background: #1A1C22;
+  color: white;
+  min-width: 1250px;
+
+}
+
+.main-nav {
+  position: fixed;
+  z-index: 9999;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 35px;
+  font-family: Trirong;
+  font-weight: 700;  
+  font-size: 1.15em;
+  letter-spacing: 1px;
+
+}
+
+nav ul {
+  list-style: none;
+  margin: 0;
+  padding: .2em 2em;
+  float: right;
+}
+
+nav ul li {
+  display: inline-block;
+  margin: 0;
+  padding: .2em .7em;
+}
+
+nav a {
+  width: 100%;
+  height: 100%;
+  color: white;
+  text-decoration: none;
+
+}
+
+nav a:hover {
+  color: rgb(220, 120, 0);
+}
+
+.nav-bg {
+    padding-bottom: 1rem;
+  content: '';
+  position: absolute;
+  display: block;
+  top: -100%;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  background: #151515;
+  transition: .45s ease-in-out;
+}
+
+.bg-hidden {
+  top: -100%;
+  opacity: 0;
+}
+
+.bg-visible {
+  top: 0;
+  opacity: 1;
+  height: 2.5rem;
+}
+
+
+body{height: 900px;}
+header {
+  background: url('http://www.autodatz.com/wp-content/uploads/2017/05/Old-Car-Wallpapers-Hd-36-with-Old-Car-Wallpapers-Hd.jpg');
+  text-align: center;
+  width: 100%;
+  height: 50rem;
+  background-size: cover;
+  background-attachment: fixed;
+  position: relative;
+  overflow: hidden;
+  border-radius: 0 0 85% 85% / 10%;
+  color: rgb(220, 120, 0);
+  text-align: center;
+  font-family: Trirong;
+  font-weight: 700;   
+  font-size: 60px;
+}
+header .overlay{
+  width: 100%;
+  height: 100%;
+  padding: 50px;
+  color: #FFF;
+  text-shadow: 1px 1px 1px #333;
+  background-image: url('./icons/technologyMain.jpg');
+  background-size: 100%;
+  overflow: hidden;
+  
+}
+
+
+.mainPlanet h1 {
+  position: absolute;
+  margin-bottom: 2rem;
+  top: 50%;
+  left: 0;
+  width: 100%;
+  padding: .3em;
+  font-size: 3em;
+  font-weight: lighter;
+}
+
+.content-wrapper {
+  width: 80%;
+
+  padding: 1em 10%;
+  box-sizing: content-box;
+  
+}
+
+.content-wrapper h1 {
+  margin: 0;
+  color: rgb(220, 120, 0);
+  margin-bottom: 2.5rem;
+}
+
+
+
+.orange-line {
+    height: 2px;
+  width: 100%; /* To span the entire width */
+  background-color: rgb(220, 120, 0);
+  margin: 30px 0px 30px 0px; /* Adjust the margin as needed */
+}
+/* carts */
+.row{
+  margin-top: 2rem;
+}
+.cart-descr{
+  height: 100%;
+  margin-bottom: 1rem;
+  border-radius: 1rem;
+  padding: 0.5rem;
+  color: white;
+  background-color:#333 ;
+  transition-duration: 0.3s;
+  position: static ;
+}
+
+.cart_btn{
+
+    width: 6rem;
+    height: 2rem;
+    cursor: pointer;
+    color: #fff;
+    background: #1A1C22;
+    font-size: 17px;
+    border-radius: 0.5rem;
+    border: none;
+}
+.cart_btn.closed{
+  background: #333640;
+  margin-bottom: 1rem;
+}
+
+
+.modal {
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.3);
+}
+
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  z-index: 1000;
+}
+.modal-content {
+  background-color: #292b32;
+  padding: 2rem 3rem 4rem 3rem;
+  border: 2px solid rgb(147, 147, 147);
+  border-radius: 5px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.3);
+  max-width: 80%;
+  max-height: 80%;
+  overflow-y: auto;
+  z-index: 999;
+}
+.modal-header {
+  display: flex;
+  justify-content: flex-end;
+}
+.m-1{
+  border-radius: 5rem;
+  padding: 0.3rem;
+  border-color: transparent;
+}
+
+</style>
---
+++ SpaceProject/src/components/footer.vue
@@ -0,0 +1,58 @@
+<template>
+<footer class="bg-dark text-center text-white">
+
+<div class="container p-4 pb-0">
+
+  <section class="mb-4">
+
+    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      ><i class="fab fa-facebook-f"><img class="footer-icon" src="./icons/icons8-github.svg" alt=""></i
+    ></a>
+
+
+    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      ><i class="fab fa-twitter"><img class="footer-icon" src="./icons/icons8-instagram.svg" alt=""></i
+    ></a>
+
+
+    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      ><i class="fab fa-google"><img class="footer-icon" src="./icons/icons8-telegram.svg" alt=""></i
+    ></a>
+
+    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      ><i class="fab fa-instagram"><img class="footer-icon" src="./icons/icons8-facebook.svg" alt=""></i
+    ></a>
+
+    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      ><i class="fab fa-linkedin-in"><img class="footer-icon" src="./icons/icons8-twitter.svg" alt=""></i
+    ></a>
+
+
+    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+      ><i class="fab fa-github"><img class="footer-icon" src="./icons/icons8-linkedin.svg" alt=""></i
+    ></a>
+  </section>
+
+</div>
+
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">Source: <a class="nasa_link" href="https://api.nasa.gov/">NASA api</a></div>
+
+</footer>
+</template>
+
+
+
+<style scoped>
+.m-1{
+  border-radius: 5rem;
+  padding: 0.3rem;
+  border-color: transparent;
+}
+.footer-icon{
+  width: 35px;
+  height: 35px;
+}
+.nasa_link{
+  color: white;
+}
+</style>
